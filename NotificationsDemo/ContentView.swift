4f29435e8938ec97c5fbd94f16fe156235ee8f89
Constantine Mars <constantine.mars@gmail.com>
2020-03-02 16:16:37 -0800

    Init

--- NotificationsDemo/ContentView.swift
+++ NotificationsDemo/ContentView.swift
@@ -12,22 +12,18 @@ struct ContentView: View {
     @State private var showingAlert = false
     @State private var granted = false
     
-    func showNotification() -> Void {
-    
-    }
-    
     var body: some View {
         VStack {
             Text("Notification Demo")
             .padding(10)
             
-            Button(action: { self.requestPermission() }) {
-                Text("Request permission")
-            }
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text("Authorization"), message: Text(granted ? "Granted" : "Denied"))
-            }
-            .padding(10)
+//            Button(action: { self.requestPermission() }) {
+//                Text("Request permission")
+//            }
+//            .alert(isPresented: $showingAlert) {
+//                Alert(title: Text("Authorization"), message: Text(granted ? "Granted" : "Denied"))
+//            }
+//            .padding(10)
             
             Button(action: { self.showNotification() }) {
                 Text("Show Notification!")
@@ -42,6 +38,39 @@ struct ContentView: View {
             .requestAuthorization(options: [.alert, .badge, .alert]) { granted, error in
                 self.granted = granted
                 self.showingAlert = true
+        }
+    }
+    
+    func showNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Title"
+            content.subtitle = "Subtitle"
+            content.sound = UNNotificationSound.default
+
+            var dateComponents = DateComponents()
+            dateComponents.second = 1
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+//            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh")
+                    }
+                }
+            }
         }
     }
 }
